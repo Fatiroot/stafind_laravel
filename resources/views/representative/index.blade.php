35ec56bd66625be9bdf68b0dc1f4ed0a77d0a0cc
Rassili <adnan.rassili47@gmail.com>
2024-04-25 22:41:10 +0100

    add view of profile in dachboard representative

--- resources/views/representative/index.blade.php
+++ resources/views/representative/index.blade.php
@@ -88,6 +88,7 @@
                         <tr
                             class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
                             <th class="px-4 py-3">Recuiter</th>
+                            <th class="px-4 py-3">Profile</th>
                             <th class="px-4 py-3">Status</th>
                             <th class="px-4 py-3">change status</th>
                         </tr>
@@ -112,7 +113,17 @@
                                         </div>
                                     </div>
                                 </td>
-
+                                <td class="px-4 py-3 text-xs">
+                                    <a href="{{ route('representative.show', $user->id) }}" class="inline-flex items-center w-full px-2 py-1 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200">
+                                        <script src="https://cdn.lordicon.com/lordicon.js"></script>
+                                        <lord-icon
+                                            src="https://cdn.lordicon.com/vfczflna.json"
+                                            colors="primary:#848484,secondary:#3080e8"
+                                            style="width:20px;height:20px; cursor: pointer;"
+                                        >
+                                        </lord-icon>
+                                    </a>
+                                </td>
                                 <td class="px-4 py-3 text-xs">
                                     <span
                                         class="px-2 py-1 font-semibold leading-tight text-red-700 bg-red-100 rounded-full dark:bg-red-700 dark:text-red-100">
